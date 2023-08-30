5c133b8618c00e2c4833b00a14beb5ceeec5aa87
AppiahBlessed <kwabenadicey@gmail.com>
2023-08-30 19:42:44 +0000

    Beety coding style

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -1,6 +1,6 @@
 #include "binary_trees.h"
 /**
- *binary_tree_height - Return depth of structure
+ *binary_tree_depth - Return depth of structure
  *@tree: Root node to start from
  *Return: The height
  *
